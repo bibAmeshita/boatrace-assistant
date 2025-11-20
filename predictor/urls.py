fb28b9688cf139cb912266b56e62352e4cf9c6de
masonames1173 <ameshita@bibandco.com>
2025-11-20 17:20:13 +0900

    Delete

--- predictor/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import race_prediction
-
-urlpatterns = [
-    path("", race_prediction, name="race_prediction"),
-]
--- predictor_2/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import race_predict
-
-urlpatterns = [
-    path("race_predict/", race_predict),
-]
